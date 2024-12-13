def8fa4f41179917fc2b0611e7f10567ad2d5634
Vitalii <vitaliti.yurchenko@gmail.com>
2024-12-13 17:06:08 +0200

    Added default method

--- src/app/ArrayUtils.java
+++ src/app/ArrayUtils.java
@@ -2,5 +2,29 @@ package app;
 
 public class ArrayUtils {
 
+    private int arraySum(int[] array) {
+        int result = 0;
+        for (int value : array) {
+            result += value;
+        }
+        return result;
+    }
 
+    public int getArraySum(int[] array) {
+        return arraySum(array);
+    }
+
+    private int maxValueArray(int[] array) {
+        int maxValue = 0;
+        for (int value : array) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+
+    public int getMaxValueArray(int[] array) {
+        return maxValueArray(array);
+    }
 }
